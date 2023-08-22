03371f20842e2b4cc4bab53521018d1c19be943f
Nickf12 <nicco.magnoni@gmail.com>
2023-08-22 11:29:59 +0200

    Add TODOs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use once_cell::sync::Lazy;
 use route::create_router;
 use tower_http::cors::CorsLayer;
 
-// secret key for JWT token
+// TODO: Add secret key for JWT token
 static _KEYS: Lazy<auth::_Keys> = Lazy::new(|| {
     let secret = std::env::var("JWT_SECRET").unwrap_or_else(|_| "Your secret here".to_owned());
     auth::_Keys::_new(secret.as_bytes())
